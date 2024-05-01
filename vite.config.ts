89762d851df7ea1def7ede5c9df0bf1fc5520f4b
p.potemkin <p.potemkin@space307.com>
2024-05-01 13:21:38 +0000

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      VueDevTools(),
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: process.env.VITE_PORT
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  })
+}
